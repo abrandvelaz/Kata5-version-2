4dfd9ad93f8deaaba7b23ecf09752681c968b44d
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-27 09:02:47 +0000

    Import the package and create the methods

--- src/Model/Histogram.java
+++ src/Model/Histogram.java
@@ -5,10 +5,30 @@
  */
 package Model;
 
-/**
- *
- * @author Alber
- */
-public class Histogram {
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+ public class Histogram<T> {
+    private final Map<T,Integer> map;
     
-}
+    public Histogram() {
+        map = new HashMap();
+    }
+    
+    public int get(T key) {
+        return map.get(key);
+    }
+    
+    public Set<T> keySet() {
+        return map.keySet();
+    }
+    
+    public void increment(T key) {
+        if (map.get(key) != null) {
+            map.put(key, map.get(key)+1);
+        } else {
+            map.put(key, 1);
+        }
+    }
+ }
