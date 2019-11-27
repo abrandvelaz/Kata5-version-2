1fe514607c3b31eca370b958edb00dbeadf7b695
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-27 10:02:14 +0000

    End the class MailListReader

--- src/View/MailListReader.java
+++ src/View/MailListReader.java
@@ -5,10 +5,31 @@
  */
 package View;
 
-/**
- *
- * @author Alber
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import Model.Mail;
+
 public class MailListReader {
     
+    public static List<Mail> read(String fileName) throws Exception {
+        List<Mail> lista = new ArrayList<>();
+        File file = new File(fileName);
+         try {
+             Scanner sc = new Scanner(file);
+             while (sc.hasNextLine()) {
+                String email = sc.nextLine();
+                if (email.indexOf('@') != -1) {
+                    lista.add(new Mail(email));
+                }
+            }
+            sc.close();
+        } 
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
 }
