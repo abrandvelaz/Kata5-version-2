db3de18d74192bd200d406c1e1c7c83b0d20922d
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-27 09:57:54 +0000

    End the class MailHistogramBuilder

--- src/View/MailHistogramBuilder.java
+++ src/View/MailHistogramBuilder.java
@@ -5,10 +5,17 @@
  */
 package View;
 
-/**
- *
- * @author Alber
- */
+import java.util.HashMap;
+import java.util.List;
+import Model.Histogram;
+import Model.Mail;
+
 public class MailHistogramBuilder {
-    
+    public static Histogram<String>  build(List<Mail> lista) {
+        Histogram<String> histogram = new Histogram<>();
+        for (int i = 0;i < lista.size();i++) {
+            histogram.increment(lista.get(i).getDomain());
+        }
+        return histogram;
+    }
 }
