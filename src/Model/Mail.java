5be7f86eae1cfc7217440c83324a22f1238657a8
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-27 10:06:53 +0000

    End the class Mail

--- src/Model/Mail.java
+++ src/Model/Mail.java
@@ -11,4 +11,18 @@ package Model;
  */
 public class Mail {
     
+    private String mail;
+    
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+    
+    public String getMail() {
+        return mail;
+    }
+    
+    public String getDomain() {
+        return mail.substring(mail.indexOf('@')+1);
+    }
+    
 }
